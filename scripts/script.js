84c2aa8ff7de55287915b5f2d147d7515b7fee25
Mateusz <75246331+Zikuz@users.noreply.github.com>
2021-06-01 22:19:36 +0200

    to-do, in-progress and done list

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,12 @@ const inputTask = document.querySelector('.add-task')
 const addBtn = document.querySelector('.add')
 const listToDo = document.querySelector('.list-todo')
 const listInProgress = document.querySelector('.list-inprogress')
+const listDone = document.querySelector('.list-done')
 
-addBtn.addEventListener('click', () => {
-    if (inputTask.value != '""' && inputTask.value != ' ') {
-        listToDo.innerHTML += `<li><span class='li'> ${inputTask.value} </span><button class='deleteBtn' onClick='deleteLi(this)'>X</button><button class='in-progress-btn'>O</button></li>`;
+addBtn.addEventListener('click', function() {
+    if (inputTask.value != 'a' && inputTask.value != ' ') {
+        listToDo.innerHTML += `
+        <li><span class='li'>${inputTask.value}</span> <button class='deleteBtn' onClick='deleteLi(this)'>X</button><button class='in-progress-btn' onClick='moveToInProgress(this)'>O</button></li>`;
         inputTask.value = ''
     } else {
         inputTask.value = ''
@@ -17,5 +19,18 @@ deleteLi = (e) => {
 }
 
 moveToInProgress = (e) => {
-
+    listInProgress.innerHTML += `<li><span class='li'>${e.parentElement.textContent}</span> <button class='deleteBtn' onClick='deleteLi(this)'>X</button><button class='doneBtn' onClick='doneLi(this)'>V</button></li>`
+    e.parentElement.remove();
 }
+
+doneLi = (e) => {
+    listDone.innerHTML += `<li><span class='li li-done'>${e.parentElement.textContent}</span> <button class='deleteBtn' onClick='deleteLi(this)'>X</button></li>`
+    e.parentElement.remove();
+}
+
+inputTask.addEventListener('keydown', (e) => {
+  if (e.keyCode === 13) {
+   e.preventDefault();
+   addBtn.click();
+  }
+});
