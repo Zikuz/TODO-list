9c3249339228a57fb7a2460a4e16196f9eb55e52
Mateusz <75246331+Zikuz@users.noreply.github.com>
2021-06-05 21:52:47 +0200

    icons

--- scripts/script.js
+++ scripts/script.js
@@ -7,25 +7,29 @@ const listDone = document.querySelector('.list-done')
 addBtn.addEventListener('click', function() {
     if (inputTask.value != '' && inputTask.value != ' ') {
         listToDo.innerHTML += `
-        <li><span class='li'>${inputTask.value}</span> <button class='deleteBtn' onClick='deleteLi(this)'>X</button><button class='in-progress-btn' onClick='moveToInProgress(this)'>O</button></li>`;
+        <li><span class='li'>${inputTask.value}</span><div class='icons1'><img src='/images/delete.svg' class='deleteBtn' onClick='deleteLi(this)'><img src='/images/in-progress.svg' class='in-progress-btn' onClick='moveToInProgress(this)'><img src='/images/done.svg' class='done-btn' onClick='doneTask(this)'></div></li>`;
         inputTask.value = ''
     } else {
         inputTask.value = ''
+        inputTask.classList.add('input-animation')
+        setTimeout(() => {
+            inputTask.classList.remove('input-animation')
+        }, 1000);
     }
 })
 
 deleteLi = (e) => {
-    e.parentElement.remove();
+    e.parentElement.parentElement.remove();
 }
 
 moveToInProgress = (e) => {
-    listInProgress.innerHTML += `<li><span class='li'>${e.parentElement.textContent}</span> <button class='deleteBtn' onClick='deleteLi(this)'>X</button><button class='doneBtn' onClick='doneLi(this)'>V</button></li>`
-    e.parentElement.remove();
+    listInProgress.innerHTML += `<li><span class='li'>${e.parentElement.parentElement.childNodes[0].innerText} </span><div class='icons2'><img src='/images/delete.svg' class='deleteBtn' onClick='deleteLi(this)'><img src='/images/done.svg' class='done-btn' onClick='doneTask(this)'></div></li>`
+    e.parentElement.parentElement.remove();
 }
 
-doneLi = (e) => {
-    listDone.innerHTML += `<li><span class='li li-done'>${e.parentElement.textContent}</span> <button class='deleteBtn' onClick='deleteLi(this)'>X</button></li>`
-    e.parentElement.remove();
+doneTask = (e) => {
+    listDone.innerHTML += `<li><span class='li task-done'>${e.parentElement.parentElement.childNodes[0].innerText} </span><div class='icons3'><img src='/images/delete.svg' class='deleteBtn' onClick='deleteLi(this)'></div></li>`
+    e.parentElement.parentElement.remove();
 }
 
 inputTask.addEventListener('keydown', (e) => {
